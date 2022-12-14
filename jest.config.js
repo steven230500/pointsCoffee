7dd5161c313211f80eab7d328a384adb173ef1b2
Steven Patiño <steven.patino@soytul.com>
2022-12-14 08:29:26 -0500

    add jest config:

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  transformIgnorePatterns: ['<rootDir>/node_modules/']
+  transformIgnorePatterns: [
+    'node_modules/(?!@react-native|react-native)'
+  ],
 };
